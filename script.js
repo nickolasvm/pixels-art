f609b467fb3997367cb1ef4c1e0f56b8673622d1
nickolasvm <nickolas64vm@gmail.com>
2022-09-01 18:43:30 -0300

    Selecionar cores e pintar o quadro de pixels

--- script.js
+++ script.js
@@ -44,16 +44,37 @@ window.onload = function() {
     randomBtn.addEventListener('click', randomColorGen);
 
     /********************************************************/
+    const color1 = document.getElementById('color-1');
+    const color2 = document.getElementById('color-2');
+    const color3 = document.getElementById('color-3');
+    const color4 = document.getElementById('color-4');
+    let colorPicked = '';
+
+    function pickColor(element) {
+        // getComputedStyle --0 get all the styles that the element hold in a object format
+        colorPicked = getComputedStyle(element.target)['backgroundColor'];
+        console.log(colorPicked);
+    }
+
+    color1.addEventListener('click', pickColor)
+    color2.addEventListener('click', pickColor)
+    color3.addEventListener('click', pickColor)
+    color4.addEventListener('click', pickColor)
 
     // populate pixel grid
     const pixelBoard = document.getElementById('pixel-board');
-    function incluirPixels(tamanho) {
+    const pixel = document.createElement('div');
+    function includePixels(tamanho) {
         for (let i = 0; i < tamanho; i += 1) {
             const pixel = document.createElement('div');
             pixel.classList.add('pixel');
             pixelBoard.appendChild(pixel);
+
+                    
+            pixel.addEventListener('mouseover', function(){
+                pixel.style.backgroundColor = colorPicked;
+            })
         }
     }
-    incluirPixels(25);
-
+    includePixels(25);
 }
