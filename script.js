7a7cdadcb78c3c14d7d3cefb29246f14a2ca157d
nickolasvm <nickolas64vm@gmail.com>
2022-09-01 17:17:00 -0300

    Criação do grid de pixels

--- script.js
+++ script.js
@@ -5,19 +5,22 @@ window.onload = function() {
     // check if localStorage is empty or not
     // if not, update the color
     if (localStorage.length !== 0) {
+        let savedColor = JSON.parse(localStorage.getItem('colorPalette'));
+        
         for (let i = 0; i < colorDiv.length; i += 1) {
-            let savedColor = localStorage.getItem(`color-${[i]}`);
-            colorDiv[i].style.backgroundColor = savedColor;
+            colorDiv[i].style.backgroundColor = savedColor[i];
         }
     }
 
-    // random number from 0 to 255
+    // random number from 1 to 255
     function randomRGB() {
         return Math.floor(Math.random() * 255 + 1);
     }
 
-    // random color from 1 to 255
+    // apply random color to the palette
     function randomColorGen() {
+        const storagePalette = [];
+
         for (let i = 0; i < colorDiv.length; i += 1) {
             let r = randomRGB();
             let g = randomRGB();
@@ -31,10 +34,26 @@ window.onload = function() {
             let randomColor = `rgb(${r}, ${g}, ${b})`
 
             colorDiv[i].style.backgroundColor = randomColor;
-            // save the color in localStorage
-            localStorage.setItem(`color-${[i]}`, randomColor);
+            // save the color in array
+            storagePalette.push(randomColor);
         }
+
+        localStorage.setItem('colorPalette', JSON.stringify(storagePalette));
     }
 
     randomBtn.addEventListener('click', randomColorGen);
+
+    /********************************************************/
+
+    // populate pixel grid
+    const pixelBoard = document.getElementById('pixel-board');
+    function incluirPixels(tamanho) {
+        for (let i = 0; i < tamanho; i += 1) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixelBoard.appendChild(pixel);
+        }
+    }
+    incluirPixels(25);
+
 }
