982bd9402d2c5cc4203fe4feccc85588b2ef5833
nickolasvm <nickolas64vm@gmail.com>
2022-09-05 13:41:46 -0300

    Função para salvar o desenho atual no localStorage

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ window.onload = function() {
     const colorDiv = document.getElementsByClassName('color-random');
     const randomBtn = document.getElementById('button-random-color');
     const clearBtn = document.getElementById('clear-board');
+    const boardDiv = document.getElementById('board-div');
 
+    // localStorage.clear()
     // check if localStorage is empty or not
     // if not, update the color
-    if (localStorage.length !== 0) {
+    if (localStorage['colorPalette'] !== undefined) {
         let savedColor = JSON.parse(localStorage.getItem('colorPalette'));
-        
         for (let i = 0; i < colorDiv.length; i += 1) {
             colorDiv[i].style.backgroundColor = savedColor[i];
         }
@@ -53,6 +54,7 @@ window.onload = function() {
 
     // make black the deafult color picked
     let colorPicked = 'rgb(0,0,0)';
+    
     function pickColor(element) {
         // getComputedStyle --0 get all the styles that the element hold in a object format
         colorPicked = getComputedStyle(element.target)['backgroundColor'];
@@ -68,45 +70,75 @@ window.onload = function() {
     }
 
     // populate pixel grid and add eventlistener for drawing
-    const pixelBoard = document.getElementById('pixel-board');
+    // 'desenhar' is set as false by default and only true when the windows and pixel event listener happens
+    let desenhar = false;
 
-
-    // 'desenhar' is set as false by default
-    let desenhar = false; 
+    // funtion to create the board and the pixels
     function includePixels(tamanho) {
-        // tamanho * tamanho to make a perfect square
-        for (let i = 0; i < tamanho * tamanho; i += 1) {
-            const pixel = document.createElement('div');
-            // set attribute draggable false to prevent dragging the paint pixels
-            pixel.setAttribute('draggable', false)
-            pixel.classList.add('pixel');
+        // create a board if it was not created before
+        const pixelBoard = document.createElement('div');
+        pixelBoard.setAttribute('draggable', false)
+        pixelBoard.setAttribute('id', 'pixel-board');
+        boardDiv.appendChild(pixelBoard);
 
-            // add eventListener to paint the board with the selected color
-            pixel.addEventListener('mouseover', function(){
-                if(!desenhar) return
-                pixel.style.backgroundColor = colorPicked;
-            })
-            pixel.addEventListener('mousedown', function(){
-                pixel.style.backgroundColor = colorPicked;
-            })
-            pixelBoard.appendChild(pixel);
+        if (localStorage['pixelBoard'] !== undefined) {
+            let savedDrawing = localStorage.getItem('pixelBoard');
+            let wraper = document.createElement('div');
+            pixelBoard.innerHTML = `${savedDrawing}`;
+
+            const pixel = document.getElementsByClassName('pixel');
+            console.log(pixel);
+            for (let i = 0; i < pixel.length; i += 1) {
+                pixel[i].addEventListener('mouseover', function(){
+                    if(!desenhar) return
+                    event.target.style.backgroundColor = colorPicked;
+                })
+                pixel[i].addEventListener('mousedown', function(){
+                    event.target.style.backgroundColor = colorPicked;
+                })
+            }
+
+        } else {
+            // tamanho * tamanho to make a perfect square
+            for (let i = 0; i < tamanho * tamanho; i += 1) {
+                const pixel = document.createElement('div');
+                // set attribute draggable false to prevent dragging the paint pixels
+                pixel.setAttribute('draggable', false)
+                pixel.classList.add('pixel');
+                // add eventListener to paint the board with the selected color
+                pixel.addEventListener('mouseover', function(){
+                    if(!desenhar) return
+                    pixel.style.backgroundColor = colorPicked;
+                })
+                pixel.addEventListener('mousedown', function(){
+                    pixel.style.backgroundColor = colorPicked;
+                })
+                pixelBoard.appendChild(pixel);
+            }
         }
     }
+    // create a default 25 pixels board (5x5)
     includePixels(5);
     // when mouse is down, 'desenhar' turn into true, allowing to draw
-    window.addEventListener("mousedown", function(){
+    // when mouse is up, save the board in localStorage
+    const pixelBoard = document.getElementById('pixel-board');
+    window.addEventListener('mousedown', function(){
         desenhar = true
     })
     // when mouse is up, 'desenhar' is set as false
-    window.addEventListener("mouseup", function(){
+    window.addEventListener('mouseup', function(){
         desenhar = false
+
+        localStorage.setItem('pixelBoard', pixelBoard.innerHTML);
     })
 
     // function to clean after Limpar is pressed
     const pixel = document.getElementsByClassName('pixel');
+
     clearBtn.addEventListener('click', function(){
         for (i = 0; i < pixel.length; i += 1) {
             pixel[i].style.backgroundColor = 'white';
         }
+        localStorage.setItem('pixelBoard', pixelBoard.innerHTML);
     })
 }
